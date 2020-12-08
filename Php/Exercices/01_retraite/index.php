632fc8263c96e0512043e5a98c9aff183675c50b
leagranjon <73827930+leagranjon@users.noreply.github.com>
2020-12-08 13:53:55 +0100

    Exercice fait

--- Php/Exercices/01_retraite/index.php
+++ Php/Exercices/01_retraite/index.php
@@ -2,35 +2,42 @@
 /*
 L'âge de la retraite est fixé à 55 ans
 
-SI l'âge fourni ($age) est supérieur à l'âge de la retraite ($retraite)
-    alors
+SI l'âge fourni est supérieur à l'âge de la retraite
     afficher (echo) "vous êtes à la retraite depuis X années"
 
 SINON SI l'âge fourni ($age) est inférieur à l'âge de la retraite ($retraite)
-    alors   
-    afficher (echo) "Il vous reste X années  avant la retraite"
-
+        SI l'âge est inférieur à 0 
+            afficher "un peu jeune pour penser à la retraite !"
+        SINON SI l'âge est inférieur à 18 
+            afficher "vous pas être né"
+        SINON 
+            afficher (echo) "Il vous reste X années  avant la retraite"
 SINON 
     alors
     afficher (echo) "La retraite c'est cette année"
-
 
 */
 
 $retraite = 55;
 
-$age = 60;
+$age = 12;
 
 
 if($age > $retraite) {
-
     $difference = ($age - $retraite);
     echo "vous êtes à la retraite depuis $difference années";
 }
 else if($age < $retraite) {
-    
-    $difference = ($retraite - $age);
-    echo "il vous reste $différence années avant la retraite";
+        if($age < 0) {
+            echo "vous pas être né";
+        }
+        else if($age < 18) {
+            echo "un peu trop jeune pour penser à la retraite !";
+        }
+        else {
+            $difference = ($retraite - $age);
+            echo "il vous reste $difference années avant la retraite";
+        }
 }
 else {
     echo "la retraite c'est cette années";
